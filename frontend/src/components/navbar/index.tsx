0d3c087dcb65a3f1a1e48c9e31f9637d2a01c521
Gilberto Silva <agilberto20111@hotmail.com>
2022-03-10 16:38:16 -0300

    Outro acerto na Navbar

--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -8,15 +8,17 @@ function NavBar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="http://github.com/agilberto"></a>
-                    <div className="dsmovie-contact-container">
-                        <GitHubIcon />
-                        <p className="dsmovie-contact-link">/agilberto</p>
-                    </div>
+                    <a href="http://github.com/agilberto">
+                        <div className="dsmovie-contact-container">
+                            <GitHubIcon />
+                            <p className="dsmovie-contact-link">/agilberto</p>
+                        </div>
+                   </a>
                 </div>
             </nav>
         </header>
     );
+    
 
 }
 
